f649c21a796845558fa8e02e168076d6e4d8a28b
Nathan Chancellor <natechancellor@gmail.com>
2019-09-26 22:52:02 -0700

    msm: camera: Remove pointless NULL checks in {__,}msm_remove_session_cmd_ack_q
    Clang warns:
    
    ../drivers/media/platform/msm/camera_v2/msm.c:625:18: warning: address
    of 'cmd_ack->command_q' will always evaluate to 'true'
    [-Wpointer-bool-conversion]
            if (!(&cmd_ack->command_q))
                ~  ~~~~~~~~~^~~~~~~~~
    ../drivers/media/platform/msm/camera_v2/msm.c:635:32: warning: address
    of 'session->command_ack_q' will always evaluate to 'true'
    [-Wpointer-bool-conversion]
            if ((!session) || !(&session->command_ack_q))
                              ~  ~~~~~~~~~^~~~~~~~~~~~~
    2 warnings generated.
    
    This matches what is present in msm-4.14.
    
    Fixes: 12d7df3314bf ("msm: camera: Add all camera drivers")
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/media/platform/msm/camera_v2/msm.c
+++ drivers/media/platform/msm/camera_v2/msm.c
@@ -622,8 +622,6 @@ static inline int __msm_remove_session_cmd_ack_q(void *d1, void *d2)
 {
 	struct msm_command_ack *cmd_ack = d1;
 
-	if (&cmd_ack->command_q == NULL)
-		return 0;
 
 	msm_queue_drain(&cmd_ack->command_q, struct msm_command, list);
 
@@ -632,7 +630,7 @@ static inline int __msm_remove_session_cmd_ack_q(void *d1, void *d2)
 
 static void msm_remove_session_cmd_ack_q(struct msm_session *session)
 {
-	if ((!session) || (&session->command_ack_q == NULL))
+	if (!session)
 		return;
 
 	mutex_lock(&session->lock);
