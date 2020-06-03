0f72e488674b766b85704f4157946cb8bd80afbd
F2LX <felixfebryan16@gmail.com>
2020-06-03 10:04:46 +0000

    Import: Compilinh script

---
+++ build-proton-old.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# Copyright © 2018-2019, "penglezos" <panagiotisegl@gmail.com>
+# Thanks to Vipul Jha for zip creator
+# Android Kernel Compilation Script
+#
+
+echo -e "==============================================="
+echo    "         Compiling Phantom CAF Kernel             "
+echo -e "==============================================="
+
+LC_ALL=C date +%Y-%m-%d
+date=`date +"%Y%m%d-%H%M"`
+BUILD_START=$(date +"%s")
+KERNEL_DIR=$PWD
+REPACK_DIR=$KERNEL_DIR/AnyKernel3
+OUT=$KERNEL_DIR/out
+VERSION="3.4"
+
+rm -rf out
+mkdir -p out
+make O=out clean
+make O=out mrproper
+make O=out ARCH=arm64 whyred-old_defconfig
+PATH="/mnt/ssd/felix/proton/bin:${PATH}" \
+make -j$(nproc --all) O=out \
+ARCH=arm64 \
+CC="ccache /mnt/ssd/felix/proton/bin/clang" \
+CROSS_COMPILE=aarch64-linux-gnu- \
+CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+
+cd $REPACK_DIR
+cp $KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb $REPACK_DIR/
+FINAL_ZIP="PhantomCAF-EAS-oldcam-${VERSION}.zip"
+zip -r9 "${FINAL_ZIP}" *
+cp *.zip $OUT
+rm *.zip
+cd $KERNEL_DIR
+rm AnyKernel3/Image.gz-dtb
+
+BUILD_END=$(date +"%s")
+DIFF=$(($BUILD_END - $BUILD_START))
+echo -e "Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+cd out
+rclone copy *.zip drive:3.4-lto
